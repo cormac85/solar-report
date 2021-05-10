7780107ed007d46faf98bdc237dd9a1be2e8a2e4
Cormac Nolan <cormacnolan85@gmail.com>
2021-05-10 22:20:54 +0100

    plot changed to value from kWh; model will now use all valid data and apply to all future months;

--- R/push_solar_estimate.R
+++ R/push_solar_estimate.R
@@ -2,29 +2,41 @@ library(dplyr)
 library(tidyr)
 library(jsonlite)
 library(ggplot2)
+library(lubridate)
 
 create_message <- function(solar_data_df){
   days_in_current_month <- Sys.Date() %>% strftime(format="%d") %>% as.numeric()
-  current_month <-  Sys.Date() %>% strftime(format="%B")
+  current_month_string <-  Sys.Date() %>% strftime(format="%B")
+  current_month <- 
+    Sys.Date() %>% 
+    strftime(format = "%Y-%m") %>% 
+    lubridate::ym()
   
   solar_value_this_month <-  
     solar_data_df %>% 
-    filter(!is_training) %>% 
+    filter(year_month == current_month) %>% 
     pull(estimated_energy_utilised_value) %>% 
     round(2)
   
   estimated_feed_in_tarrif_this_month <-
     solar_data_df %>% 
-    filter(!is_training) %>% 
+    filter(year_month == current_month) %>% 
     pull(estimated_feed_in_tarrif_value) %>% 
     round(2)
     
-    
-    round(filter(solar_data_df, !is_training)$estimated_energy_utilised_value, 2)
   
   confidence_inteval <- 
-    (filter(solar_data_df, !is_training)$estimated_energy_utilised_value 
-     - filter(solar_data_df, !is_training)$lower_confint_value)
+    (
+      filter(
+        solar_data_df, 
+        year_month == current_month
+      )$estimated_energy_utilised_value 
+      - 
+      filter(
+        solar_data_df, 
+        year_month == current_month
+        )$lower_confint_value
+    )
   
   list(
     title_text = paste0(
@@ -34,7 +46,7 @@ create_message <- function(solar_data_df){
       " (± €", round(confidence_inteval, 2), ")"
     ), 
     body_text = paste0(
-      current_month, " Mean Value per Day: €",
+      current_month_string, " Mean Value per Day: €",
       round(solar_value_this_month / days_in_current_month, 2),
       "\n",
       "Estimated Feed In Tarrif: €", estimated_feed_in_tarrif_this_month,
@@ -50,7 +62,7 @@ create_message <- function(solar_data_df){
 # Constants
 
 ELECTRICITY_PRICE = 0.20
-FEED_IN_TARRIF_PRICE = 0.15
+FEED_IN_TARRIF_PRICE = 0.1
 ESTIMATED_SELF_USE_RATE = 0.75
 
 
@@ -61,7 +73,7 @@ monthly_solar_generation <- dplyr::tibble(
   year = c(rep("2021", 12), rep("2020", 12), rep("2019", 12), rep("2018", 12)),
   month = rep(1:12, 4),
   solar_power_generation = c(
-    c(85, 157.5, 280.7, 70.0, rep(NA, 8)),
+    c(85, 157.5, 280.7, 471.6, rep(NA, 8)),
     c(rep(NA, 5), 358.4, 394.9, 314.3, 332.8, 235, 119.6, 38.7),
     rep(NA, 12),
     rep(NA, 12)
@@ -76,7 +88,9 @@ dunsany_solar_radiation <-
   mutate(year=c("2021", "2020", "2019", "2018", "mean")) %>% 
   select(-annual) %>% 
   filter(year != "mean") %>% 
-  tidyr::pivot_longer(january:december, names_to = "month", values_to = "solar_irradiation_jpercm2") %>% 
+  tidyr::pivot_longer(
+    january:december, names_to = "month", values_to = "solar_irradiation_jpercm2"
+  ) %>% 
   mutate(solar_irradiation_jpercm2 = dplyr::na_if(solar_irradiation_jpercm2, ""),
          solar_irradiation_jpercm2 = dplyr::na_if(solar_irradiation_jpercm2, "n/a"),
          month = rep(1:12, 4),
@@ -88,23 +102,24 @@ solar_data <-
 
 solar_data <- 
   solar_data %>% 
-  filter(!is.na(solar_irradiation_jpercm2),
-         !is.na(solar_power_generation))
+  filter(!is.na(solar_irradiation_jpercm2)) %>% 
+  tidyr::unite("year_month", c(year, month), sep="-") %>% 
+  mutate(year_month = lubridate::ym(year_month)) %>% 
+  filter(year_month >= "2020-06-01")
 
 solar_data <- 
   solar_data %>% 
-  mutate(month = sprintf("%02d", as.numeric(month))) %>% 
-  mutate(year = as.integer(year)) %>% 
-  tidyr::unite("year_month", year, month, sep="-", remove=FALSE) %>% 
   mutate(
-    normalised_irradiation = solar_irradiation_jpercm2 / max(solar_irradiation_jpercm2),
-    normalised_solar_power = solar_power_generation / max(solar_power_generation)
+    normalised_irradiation = solar_irradiation_jpercm2 / 
+      max(solar_irradiation_jpercm2, na.rm = TRUE),
+    normalised_solar_power = solar_power_generation / 
+      max(solar_power_generation, na.rm = TRUE)
   ) %>% 
   arrange(year_month)
 
 solar_data <- 
   solar_data %>% 
-  mutate(is_training = ifelse(year_month != max(year_month), TRUE, FALSE))
+  mutate(is_training = !is.na(normalised_solar_power))
 
 # Train
 
@@ -146,31 +161,50 @@ push_message <- create_message(solar_data)
 
 generation_plot <- 
   solar_data %>% 
-  select(year_month, solar_power_generation, fitted_values) %>% 
-  tidyr::pivot_longer(cols = solar_power_generation:fitted_values,
-                      names_to = "energy_type",
-                      values_to = "kWh") %>% 
-  mutate(energy_type = factor(energy_type, levels = c("solar_power_generation", "fitted_values"))) %>% 
-  ggplot(aes(year_month, kWh, 
-             colour = energy_type, group = energy_type,
-             linetype = energy_type)) +
-  geom_line(size = 1.2) +
+  select(
+    year_month, c(estimated_energy_utilised_value, estimated_feed_in_tarrif_value)
+  ) %>% 
+  tidyr::pivot_longer(
+    cols = c(estimated_energy_utilised_value, estimated_feed_in_tarrif_value),
+    names_to = "value_type",
+    values_to = "euro"
+  ) %>% 
+  mutate(energy_type = factor(
+    value_type, 
+    levels = c("estimated_feed_in_tarrif_value", "estimated_energy_utilised_value"))
+  ) %>% 
+  ggplot(aes(
+    year_month, 
+    euro, 
+    fill = forcats::fct_rev(value_type), 
+    group = forcats::fct_rev(value_type)
+  )) +
+  geom_bar(size = 1.2, stat="identity") +
   theme_light() +
   theme(panel.grid.minor.x = element_blank(),
         text = element_text(size = 18),
         axis.text.x = element_text(angle = 45, hjust = 1), 
         legend.position = "top") +
-  scale_colour_brewer(type = "qual", palette = "Set2") +
+  scale_fill_brewer(
+    type = "qual", 
+    palette = "Set2", 
+    labels = c(
+      paste0("Feed In Tarrif at ", 100 * FEED_IN_TARRIF_PRICE, "c"), "Utilised"
+    )
+  ) +
   labs(
-    title = paste0("Solar Energy Production - Fitted vs Actual"),
+    title = paste0("Estimated Solar Energy Value Realised - Feed In Tarrif & Utilised"),
     x = "Year-Month",
-    y = "kWh",
-    colour = "Actual vs Model",
-    linetype= "Actual vs Model"
+    y = "Euro",
+    fill = "Estimated Value Per Month (Euro)"
   )
 
-ggsave("generation_plot.png", plot = generation_plot,
-       width = 8, height = 4.5)
+ggsave(
+  "generation_plot.png", 
+  plot = generation_plot,
+  width = 12, 
+  height = 6.75
+)
 
 RPushbullet::pbPost(
   "file",
